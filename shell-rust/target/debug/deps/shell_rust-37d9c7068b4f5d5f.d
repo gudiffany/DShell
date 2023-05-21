c5b189cebb3b422d4755237190ce2ecd38c67fc4
gudiffany <565736584@qq.com>
2023-05-21 21:34:34 +0800

    Delete shell-rust/target directory

--- shell-rust/target/debug/deps/shell_rust-37d9c7068b4f5d5f.d
+++
@@ -1,6 +0,0 @@
-/home/diffany/桌面/shell/shell-rust/target/debug/deps/shell_rust-37d9c7068b4f5d5f.rmeta: src/main.rs src/srceen.rs
-
-/home/diffany/桌面/shell/shell-rust/target/debug/deps/shell_rust-37d9c7068b4f5d5f.d: src/main.rs src/srceen.rs
-
-src/main.rs:
-src/srceen.rs:
