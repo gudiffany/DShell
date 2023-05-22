f065d6157c3315da8e5a4ad2316c7732113cb0d6
gudiffany <565736584@qq.com>
2023-05-22 12:03:00 +0800

    Update command.rs
    add-cat-command

--- shell-rust/src/command.rs
+++ shell-rust/src/command.rs
@@ -4,33 +4,31 @@ pub struct Command {}
 impl Command {
     pub fn output_command(command_name: String, parts: Vec<String>) {
         match command_name.as_str() {
-            
             "ls" => {
+                let mut current_dir = env::current_dir().expect("Failed to get current directory.");
                 if parts.len() > 1 {
-                    let current_dir = env::current_dir().expect("Failed to get current directory.");
-                    let directory_path = current_dir.join(&parts[1]);
-                    let entries = fs::read_dir(directory_path).expect("Failed to read directory.");
-                    for entry in entries {
-                        if let Ok(entry) = entry {
-                            let file_name = entry.file_name();
-                            let file_name = file_name.to_string_lossy();
-                            println!("{}", file_name);
-                        }
-                    }
-                } else {
-                    let current_dir = env::current_dir().expect("Failed to get current directory.");
-                    let entries = fs::read_dir(current_dir).expect("Failed to read directory.");
-                    for entry in entries {
-                        if let Ok(entry) = entry {
-                            let file_name = entry.file_name();
-                            let file_name = file_name.to_string_lossy();
-                            println!("{}", file_name);
-                        }
+                    current_dir = current_dir.join(&parts[1]);
+                }
+                let entries = fs::read_dir(current_dir).expect("Failed to read directory.");
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        let file_name = entry.file_name();
+                        let file_name = file_name.to_string_lossy();
+                        println!("{}", file_name);
                     }
                 }
             }
-
-
+            "cat" => {
+                if parts.len() > 1 {
+                    if let Ok(readfile) = fs::read_to_string(&parts[1]) {
+                        println!("{}", readfile);
+                    } else {
+                        println!("Error: File not found or unable to read");
+                    }
+                } else {
+                    println!("No filename provided");
+                }
+            }
             "exit" => {
                 println!("success exit");
                 std::process::exit(1);
