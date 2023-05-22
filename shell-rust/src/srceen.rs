593dd548eaf444358f72b3f9a71b76210e437a63
gudiffany <565736584@qq.com>
2023-05-22 12:47:48 +0800

    Update srceen.rs
    change-and-add-history

--- shell-rust/src/srceen.rs
+++ shell-rust/src/srceen.rs
@@ -6,24 +6,24 @@ pub struct Srceen {}
 
 impl Srceen {
     pub fn start(&mut self) {
-        let username = "diffany".red();
-        print!("{} @ diffany-virtual-machine:~$", username);
-        io::stdout().flush().expect("Failed to flush stdout");
-        self.input_command();
-    }
+        let mut commands = Command::new();
+        loop {
+            let username = "diffany".red();
+            print!("{} @ diffany-virtual-machine:~$", username);
+            io::stdout().flush().expect("Failed to flush stdout");
+            let mut command = String::new();
 
-    fn input_command(&mut self) {
-        let mut command = String::new();
-        io::stdin()
-            .read_line(&mut command)
-            .expect("Failed to read input");
+            io::stdin()
+                .read_line(&mut command)
+                .expect("Failed to read input");
 
-        let current_command = command.trim();
-        let parts: Vec<String> = current_command
-            .split(' ')
-            .map(|part| part.to_string())
-            .collect();
-        let command_name = &parts[0];
-        Command::output_command(command_name.to_string(), parts)
+            let current_command = command.trim();
+            let parts: Vec<String> = current_command
+                .split(' ')
+                .map(|part| part.to_string())
+                .collect();
+            let command_name = &parts[0];
+            commands.output_command(command_name.to_string(), parts)
+        }
     }
 }
